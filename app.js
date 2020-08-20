e2a4fd7b977b51c91f4923a8087a2a7091554904
Chuks Ossai <ossaiwisdom48@gmail.com>
2020-08-20 22:29:38 +0100

    Add Admin LTE starter template and configure layouts for front and back views

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const ejsLayouts = require('express-ejs-layouts');
 
-var app = express();
+const app = express();
 
 // view engine setup
+app.set('layout', './layouts/front');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -17,7 +19,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(ejsLayouts);
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
